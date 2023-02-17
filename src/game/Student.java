f925e1f17ca4db5539bf2f82a770dc6c0d23f20d
vis <48868398+vis2x@users.noreply.github.com>
2023-02-17 13:38:07 +0000

    Added Student credit attribute

--- src/game/Student.java
+++ src/game/Student.java
@@ -7,12 +7,18 @@ public class Student extends DynamicBody {
     private static final Shape studentShape = new BoxShape(1, 2);
     private static final BodyImage image = new BodyImage("data/student.png", 4);
 
-
+    private int credit;
     public Student(World world) {
         super(world);
         addImage(image);
-
+        credit = 0;
     }
 
+    public int getCredit() {
+        return credit;
+    }
 
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
 }
