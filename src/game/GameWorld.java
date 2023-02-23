44b4ffb81270e36c4b55956087a9b03e5e4b59ab
vis <48868398+vis2x@users.noreply.github.com>
2023-02-23 23:47:52 +0000

    Fixed GameWorld class errors

--- src/game/GameWorld.java
+++ src/game/GameWorld.java
@@ -3,24 +3,26 @@ package game;
 import city.cs.engine.BoxShape;
 import city.cs.engine.Shape;
 import city.cs.engine.StaticBody;
+import city.cs.engine.World;
 import org.jbox2d.common.Vec2;
 
 public class GameWorld extends World {
 
     public GameWorld() {
         super();
+
         //make a ground platform
         Shape shape = new BoxShape(30, 0.5f);
-        StaticBody ground = new StaticBody(world, shape);
+        StaticBody ground = new StaticBody(this, shape);
         ground.setPosition(new Vec2(0f, -11.5f));
 
         // make a suspended platform
         Shape platformShape = new BoxShape(3, 0.5f);
-        StaticBody platform1 = new StaticBody(world, platformShape);
+        StaticBody platform1 = new StaticBody(this, platformShape);
         platform1.setPosition(new Vec2(-8, -4f));
 
         //make a character (with an overlaid image)
-        Student student = new Student(world);
+        Student student = new Student(this);
         student.setPosition(new Vec2(4,-5));
         student.setCredits(15);
     }
