227f81d3f1750fee858fe25331ebf1ae4b4bc0e0
vis <48868398+vis2x@users.noreply.github.com>
2023-03-06 03:01:22 +0000

    Added background image

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -1,17 +1,22 @@
 package game;
 
 import city.cs.engine.UserView;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GameView extends UserView {
+
     private final Image background;
+
     public GameView(GameWorld world, int width, int height) {
         super(world, width, height);
-        background = new ImageIcon("data/university.gif").getImage();
+        background = new ImageIcon("data/background/example.png").getImage();
     }
+
     @Override
     protected void paintBackground(Graphics2D g) {
         g.drawImage(background, 0, 0, this);
     }
+
 }
