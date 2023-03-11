90e206a1581f8d72ea05ff4ecbbdbafac0f65c18
vis <48868398+vis2x@users.noreply.github.com>
2023-03-11 18:49:41 +0000

    feat: set images for jump, sprint, idle

--- src/game/MainCharacter.java
+++ src/game/MainCharacter.java
@@ -5,7 +5,7 @@ import city.cs.engine.*;
 public class MainCharacter extends Walker {
 
     private static final Shape studentShape = new PolygonShape(0.08f,1.38f, -0.77f,0.94f, -0.64f,-1.81f, 0.13f,-1.78f, 0.71f,0.43f, 0.65f,1.33f, 0.12f,1.4f);
-    private static final BodyImage image = new BodyImage("data/maskdude_idle.gif", 4f);
+    private BodyImage image = new BodyImage("data/maskdude.gif", 4f);
     private static int lives;
 
     public MainCharacter(GameWorld world) {
@@ -15,20 +15,44 @@ public class MainCharacter extends Walker {
         lives = 0;
     }
 
+
+    public void setImage(String type){
+        removeAllImages();
+
+        switch (type) {
+            case "run right" -> {
+                image = new BodyImage("data/maskdude_run.gif", 4f);
+                addImage(image);
+            }
+            case "run left" -> {
+                image = new BodyImage("data/maskdude_run_mirror.gif", 4f);
+                addImage(image);
+            }
+            case "jump" -> {
+                image = new BodyImage("data/maskdude_jump.png", 4f);
+                addImage(image);
+            }
+            case "default" -> {
+                image = new BodyImage("data/maskdude_idle.gif", 4f);
+                addImage(image);
+            }
+        }
+    }
+
     public static int getLives() {
         return lives;
     }
 
-    public void setLives(int lives) {
+    public static void setLives(int lives) {
         MainCharacter.lives = lives;
     }
 
-    public void addLife(){
+    public static void addLife(){
         setLives(getLives() + 1);
         System.out.println("Apple/Life count: " + getLives());
     }
 
-    public void removeLife(){
+    public static void removeLife(){
         setLives(getLives() - 1);
     }
 }
--- src/game/MainCharacterController.java
+++ src/game/MainCharacterController.java
@@ -8,14 +8,13 @@ public class MainCharacterController implements KeyListener {
     MainCharacter mainCharacter;
     int speed = 5;
 
+
     public MainCharacterController(MainCharacter mainCharacter) {
         this.mainCharacter = mainCharacter;
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-
-
     }
 
     @Override
@@ -24,19 +23,24 @@ public class MainCharacterController implements KeyListener {
 
         if (code == KeyEvent.VK_A) {
             mainCharacter.startWalking(-speed);
+            mainCharacter.setImage("run left");
         } else if (code == KeyEvent.VK_D) {
             mainCharacter.startWalking(speed);
+            mainCharacter.setImage("run right");
+        } else if (code == KeyEvent.VK_SPACE) {
+            mainCharacter.jump(5);
+            mainCharacter.setImage("jump");
         } else if (code == KeyEvent.VK_SHIFT) {
             if (speed == 5) {
                 speed = 10;
             } else if (speed == -5) {
                 speed = -10;
             }
+        } else {
+            mainCharacter.setImage("default");
         }
 
-        if (code == KeyEvent.VK_SPACE) {
-            mainCharacter.jump(5);
-        }
+
 
     }
 
