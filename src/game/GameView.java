6e2e63f21f3b6a022bbe7168842784425d851862
vis <48868398+vis2x@users.noreply.github.com>
2023-03-11 02:21:41 +0000

    feat: main character health counter

--- src/game/GameView.java
+++ src/game/GameView.java
@@ -20,4 +20,19 @@ public class GameView extends UserView {
         g.drawImage(background, 0, 0, 700, 700, this);
     }
 
+
+    @Override
+    protected void paintForeground(Graphics2D g){
+
+        if (MainCharacter.getLives() == 3){
+            g.drawImage(new ImageIcon("data/hearts/3 hearts.png").getImage(), 317, 0, 66, 22, this);
+        } else if (MainCharacter.getLives() == 2){
+            g.drawImage(new ImageIcon("data/hearts/2 hearts.png").getImage(), 317, 0,  66, 22,this);
+        } else if (MainCharacter.getLives() == 1){
+            g.drawImage(new ImageIcon("data/hearts/1 heart.png").getImage(), 317, 0, 66, 22, this);
+        } else if (MainCharacter.getLives() == 0){
+            g.drawImage(new ImageIcon("data/hearts/0 hearts.png").getImage(), 317, 0,  66, 22,this);
+        }
+    }
+
 }
