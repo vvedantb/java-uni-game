abe8fec061556fcec06a819422b581d9afc6bd99
vis <48868398+vis2x@users.noreply.github.com>
2023-03-27 15:38:25 +0100

    feat: added level 1

---
+++ src/game/Level1.java
@@ -0,0 +1,58 @@
+package game;
+
+import city.cs.engine.BoxShape;
+import city.cs.engine.Shape;
+import city.cs.engine.StaticBody;
+import org.jbox2d.common.Vec2;
+
+import java.awt.*;
+
+public class Level1 extends GameLevel {
+
+    public Level1(Game game){
+        super(game);
+        // Ground border
+        Shape horizontalBar = new BoxShape(30, 0.5f);
+        StaticBody ground = new StaticBody(this, horizontalBar);
+        ground.setPosition(new Vec2(0f, -18f));
+
+        // Side borders
+        Shape verticalBar = new BoxShape(0.5f, 30);
+
+        // Left border
+        StaticBody leftBar = new StaticBody(this, verticalBar);
+        leftBar.setPosition(new Vec2(-18f, -Window.HEIGHT));
+
+        // Right border
+        StaticBody rightBar = new StaticBody(this, verticalBar);
+        rightBar.setPosition(new Vec2(18f, Window.HEIGHT));
+
+
+        // Tree
+        new Tree(this).setPosition(new Vec2(-15f, 6f));
+
+
+        // Platforms
+        new Platform(this).setPosition(new Vec2(-8, 0));
+        new Lift(this).setPosition(new Vec2(0, -2));
+        new Platform(this).setPosition(new Vec2(6, 0));
+
+
+        // Grass blocks
+        new Block(this, "grass").setPosition(new Vec2(-15f, 0f));
+        new Block(this, "grass").setPosition(new Vec2(15f, 0f));
+
+        // Dirt blocks
+        new Block(this, "dirt").setPosition(new Vec2(-15f, -15f));
+        new Block(this, "dirt").setPosition(new Vec2(-10f, -15f));
+        new Block(this, "dirt").setPosition(new Vec2(-5f, -15f));
+        new Block(this, "dirt").setPosition(new Vec2(0f, -15f));
+        new Block(this, "dirt").setPosition(new Vec2(5f, -15f));
+        new Block(this, "dirt").setPosition(new Vec2(10f, -15f));
+        new Block(this, "dirt").setPosition(new Vec2(15f, -15f));
+
+        GenericCollisionListener cl = new GenericCollisionListener(getPlayer());
+        getPlayer().addCollisionListener(cl);
+    }
+
+}
