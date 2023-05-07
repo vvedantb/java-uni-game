2bd449d02af26872cd9c7b0b8b675ccd0354b081
vis <48868398+vis2x@users.noreply.github.com>
2023-05-07 08:38:15 +0100

    refactored door class code

--- src/game/Door.java
+++ src/game/Door.java
@@ -4,25 +4,28 @@ import city.cs.engine.BodyImage;
 import city.cs.engine.BoxShape;
 import city.cs.engine.Shape;
 import city.cs.engine.StaticBody;
-import org.jbox2d.common.Vec2;
 
 public class Door extends StaticBody {
-    private static Shape doorShape = new BoxShape(1.0f, 1.5f);
+    private static final Shape doorShape = new BoxShape(1.0f, 1.5f);
     private BodyImage image;
-    private GameLevel level;
 
-    public Door(Game game, GameLevel level){
+    public Door(Game game, GameLevel level) {
         super(level, doorShape);
-        image = new BodyImage("data/door/closed.png", 5f);
-        addImage(image);
+        setBodyImage("closed");
         addCollisionListener(new GenericCollisionListener(level, game));
-        this.level = level;
     }
 
-    public void changeBodyImage(){
-        this.removeAllImages();
-        image = new BodyImage("data/door/open.png", 5f);
-        this.addImage(image);
+    public void setBodyImage(String type) {
+        removeAllImages();
+        if (type.equals("open")) {
+            System.out.println("Open door");
+            image = new BodyImage("data/door/open.png", 5f);
+            addImage(image);
+        } else if (type.equals("closed")) {
+            System.out.println("Closed door");
+            image = new BodyImage("data/door/closed.png", 5f);
+            addImage(image);
+        }
     }
 
 }
