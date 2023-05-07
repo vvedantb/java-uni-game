b8e8132f65866c8e49514d02f57e8493e0e10cde
vis <48868398+vis2x@users.noreply.github.com>
2023-05-07 11:08:25 +0100

    feat: mute SoundFX checkbox in GUI

--- src/game/ControlPanel.java
+++ src/game/ControlPanel.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 
 public class ControlPanel {
     public JPanel mainPanel;
-    private JButton startButton;
     private JButton quitButton;
     private JButton saveGameButton;
     private JButton loadGameButton;
@@ -19,12 +18,6 @@ public class ControlPanel {
 
     public ControlPanel(Game game, GameLevel level) {
 
-        startButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Game started!");
-            }
-        });
         quitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -73,17 +66,17 @@ public class ControlPanel {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 if (e.getStateChange() == ItemEvent.SELECTED){
-                    Game.objective_complete_sound.setVolume(0);
-                    Game.game_over_sound.setVolume(0);
-                    Coin.coinSound.setVolume(0);
-                    GenericCollisionListener.game_over_sound.setVolume(0);
-                    GenericCollisionListener.low_on_health_sound.setVolume(0);
+                    Game.objective_complete_sound.setVolume(0.1);
+                    Game.game_over_sound.setVolume(0.1);
+                    Coin.coinSound.setVolume(0.1);
+                    GenericCollisionListener.game_over_sound.setVolume(0.1);
+                    GenericCollisionListener.low_on_health_sound.setVolume(0.1);
                 } else {
-                    Game.objective_complete_sound.setVolume(1);
-                    Game.game_over_sound.setVolume(1);
-                    Coin.coinSound.setVolume(1);
-                    GenericCollisionListener.game_over_sound.setVolume(1);
-                    GenericCollisionListener.low_on_health_sound.setVolume(1);
+                    Game.objective_complete_sound.setVolume(1.0);
+                    Game.game_over_sound.setVolume(1.0);
+                    Coin.coinSound.setVolume(1.0);
+                    GenericCollisionListener.game_over_sound.setVolume(1.0);
+                    GenericCollisionListener.low_on_health_sound.setVolume(1.0);
                 }
             }
         });
